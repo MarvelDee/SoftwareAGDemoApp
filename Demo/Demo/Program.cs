9eb681454bea75bd9d423e324b8c1a89d9226638
ndelante <ndelante@lenovo.com>
2023-03-09 18:01:10 +0900

    calculator

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -1,4 +1,36 @@
-﻿
-string[] data = { "a", "b" };
+﻿Console.WriteLine("Enter a number:");
+int number = int.Parse(Console.ReadLine());
 
-Console.WriteLine(data[2]);
+Console.WriteLine("Enter an operator (+, -, *, /):");
+string op = Console.ReadLine();
+
+Console.WriteLine("Enter another number:");
+int number2 = int.Parse(Console.ReadLine());
+
+int result;
+
+if (op == "+")
+{
+    result = number + number2;
+}
+else if (op == "-")
+{
+    result = number - number;
+}
+else if (op == "*")
+{
+    result = number * number2;
+}
+else if (op == "/")
+{
+    result = number / number2;
+}
+else
+{
+    Console.WriteLine("Invalid operator.");
+    return;
+}
+
+Console.WriteLine("The result is: " + result);
+
+Console.ReadKey();
