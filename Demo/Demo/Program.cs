5a9f0936396e142d9c8e5dd1f5297eabbad8a3ef
ndelante <ndelante@lenovo.com>
2023-03-09 17:08:45 +0900

    asdsad

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -1,8 +1,4 @@
 ﻿
-Console.WriteLine("Hello World1");
-Console.WriteLine("Hello World2");
-Console.WriteLine("Hello World3");
-Console.WriteLine("Hello World4");
-Console.WriteLine("Hello World5");
-Console.WriteLine("Hello World6")
-    Console.WriteLine(
+string[] data = { "a", "b" };
+
+Console.WriteLine(data[2]);
