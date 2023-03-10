04a3d4ea948a64edb6bfeab2ea2a6e0cd090ef14
ndelante <ndelante@lenovo.com>
2023-03-10 15:15:02 +0900

    remove space

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -2,10 +2,8 @@
         int[] numbers = { 5, 2, 8, 3, 1 };
         int temp;
         
-        // outer loop
         for (int i = 0; i < numbers.Length; i++) {
 
-            // inner loop
             for (int j = i + 1; j < numbers.Length-1; j++) {
                 if (numbers[i] > numbers[j]) {
                     temp = numbers[i];
@@ -13,7 +11,6 @@
                     numbers[j] = temp;
                 }
             }
-
         }
         
         Console.WriteLine("Sorted numbers: ");
