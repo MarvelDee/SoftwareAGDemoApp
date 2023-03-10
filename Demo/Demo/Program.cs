919a2a0cca04a20833b7c58e082022ba99f2dece
ndelante <ndelante@lenovo.com>
2023-03-10 15:58:22 +0900

    sample

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -2,9 +2,9 @@
         int[] numbers = { 5, 2, 8, 3, 1 };
         int temp;
         
-        for (int i = 0; i < numbers.Length; i++) {
+        for (int i = 0; i < numbers.Length-1; i++) {
 
-            for (int j = i + 1; j < numbers.Length-1; j++) {
+            for (int j = i + 1; j < numbers.Length; j++) {
                 if (numbers[i] > numbers[j]) {
                     temp = numbers[i];
                     numbers[i] = numbers[j];
@@ -12,7 +12,6 @@
                 }
             }
         }
-        
         Console.WriteLine("Sorted numbers: ");
         for (int i = 0; i < numbers.Length; i++) {
             Console.WriteLine(numbers[i]);
